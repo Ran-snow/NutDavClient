c8e08b760c62a20c0cda3bc4a6a97ada385d7c66
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-11-21 21:57:46 +0800

    添加项目文件。

---
+++ NutDavClient/MainFrm.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace NutDavClient
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // MainFrm
+            // 
+            this.ClientSize = new System.Drawing.Size(973, 655);
+            this.Name = "MainFrm";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetting;
+    }
+}
