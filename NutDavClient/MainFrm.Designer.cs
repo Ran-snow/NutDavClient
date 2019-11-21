f881030ebac10de3b8955541658c8fd51f6738ad
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-11-21 23:06:30 +0800

    Treeview bind explorer.

--- NutDavClient/MainFrm.Designer.cs
+++ NutDavClient/MainFrm.Designer.cs
@@ -28,16 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tvFiles = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(313, 0);
+            this.listView1.Name = "lvFileExplorer";
+            this.listView1.Size = new System.Drawing.Size(660, 607);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(3, 628);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(970, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(93, 557);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sync";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tvFiles
+            // 
+            this.tvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tvFiles.Location = new System.Drawing.Point(3, 0);
+            this.tvFiles.Name = "tvFiles";
+            this.tvFiles.Size = new System.Drawing.Size(304, 536);
+            this.tvFiles.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 608);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tip";
             // 
             // MainFrm
             // 
             this.ClientSize = new System.Drawing.Size(973, 655);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tvFiles);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
             this.Name = "MainFrm";
+            this.Text = "Nut Webdav";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView tvFiles;
+        private System.Windows.Forms.Label label1;
     }
 }
--- NutDavClient/MainFrm.cs
+++ NutDavClient/MainFrm.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using NutDavClient.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.Resources;
+using System.Security.AccessControl;
 using System.Text;
 using System.Windows.Forms;
 
@@ -13,6 +17,56 @@ namespace NutDavClient
         public MainFrm()
         {
             InitializeComponent();
+        }
+
+        private void PopulateTreeView()
+        {
+            ImageList imageList = new ImageList();
+            imageList.Images.Add("ClosedFolder", (Image)(Resources.ResourceManager.GetObject("ClosedFolder")));
+            imageList.Images.Add("OpenFolder", (Image)(Resources.ResourceManager.GetObject("OpenFolder")));
+
+            tvFiles.ImageList = imageList;
+
+            TreeNode rootNode;
+
+            DirectoryInfo info = new DirectoryInfo(@"F:\Download");
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                tvFiles.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                if (subDir.Attributes.HasFlag(FileAttributes.Hidden) || subDir.GetAccessControl().AreAccessRulesProtected)
+                {
+                    continue;
+                }
+
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "ClosedFolder";
+                aNode.SelectedImageKey = "OpenFolder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            PopulateTreeView();
         }
     }
 }
