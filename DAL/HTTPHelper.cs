ab265f44bc6e6cbae38bad29d9ffa3c98dd815fc
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-11-26 17:08:10 +0800

    HTTP Client

--- DAL/HTTPHelper.cs
+++ DAL/HTTPHelper.cs
@@ -1,25 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class HTTPHelper
+    public static class HTTPHelper
     {
-        private async Task Down1oadBlock(int id, long start, long end)
+        public async static Task CrazyDowmload(string uri)
         {
-            await using FileStream file = new FileStream("{id}.dat", FileMode.Create);
+            using var httpCtient = new HttpClient();
+            using var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(uri),
+                Method = HttpMethod.Head,
+            };
+
+            var response = await httpCtient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            long? contentLength = response.Content.Headers.ContentLength;
+
+            if (contentLength == null)
+            {
+                return;
+            }
+
+            List<Task> tasks = new List<Task>();
+            long step = 1L << 20;
+
+            var blockCount = Math.Ceiling(contentLength.Value * 1.0 / step);
+
+            for (int i = 0; i < blockCount; i++)
+            {
+                long start = i * step;
+                long end = (i + 1) * step - 1;
+                if ((contentLength.Value - 1) < end)
+                {
+                    end = contentLength.Value - 1;
+                }
+
+                tasks.Add(Down1oadBlock(i, uri, step, start, end));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine(contentLength);
+        }
+
+        private async static Task Down1oadBlock(int id, string uri, long step, long start, long end)
+        {
+            await using FileStream file = new FileStream($"{id}.dat", FileMode.Create);
 
             using var httpCtient = new HttpClient();
             using var request = new HttpRequestMessage
             {
-                RequestUri = new Uri("https://www.baidu.com"),
+                RequestUri = new Uri(uri),
                 Method = HttpMethod.Get,
                 Headers =
                 {
-                    Range = new RangeHeaderValue (start,end)
+                    Range = new RangeHeaderValue(start,end)
                 }
             };
 
@@ -27,13 +69,17 @@ namespace DAL
             using var stream = await response.Content.ReadAsStreamAsync();
             var length = end - start + 1;
             var consumed = 0;
-            var buffer = new byte[1024 * 100];
-            while (consumed<length)
+            var buffer = new byte[step];
+            while (consumed < length)
             {
                 var read = await stream.ReadAsync(buffer);
                 await file.WriteAsync(buffer, 0, read);
                 consumed += read;
+
+                Console.WriteLine($"ID:{id} \t Thread:{Thread.CurrentThread.ManagedThreadId} \t {Math.Round(consumed * 1.0 / length, 2) * 100}%");
             }
+
+            Console.WriteLine($"ID:{id} 完成");
         }
     }
 }
