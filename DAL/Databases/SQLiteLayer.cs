ce3448cd4b16117fadb73e23aedfb3098d32d2b8
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-12-12 22:54:36 +0800

    add databaselayer

---
+++ DAL/Databases/SQLiteLayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Databases
+{
+    public class SQLiteLayer<T> where T : class, new()
+    {
+        ConcurrentDictionary<string, T> dictionary = new ConcurrentDictionary<string, T>();
+
+        public bool Save(string key, T content)
+        {
+            return dictionary.TryAdd(key, content);
+        }
+
+        public bool Update(string key, T newValue)
+        {
+            if (dictionary.TryGetValue(key, out T comparisonValue))
+                return dictionary.TryUpdate(key, newValue, comparisonValue);
+            else
+                return false;
+        }
+
+        public bool Delete(string key)
+        {
+            return dictionary.TryRemove(key, out T value);
+        }
+
+        public List<T> QueryALL<TKey>(Func<T, TKey> keySelector)
+        {
+            List<T> list = new List<T>();
+
+            foreach (var item in dictionary.ToArray())
+            {
+                list.Add(item.Value);
+            }
+
+            return list.OrderBy(keySelector).ToList();
+        }
+
+        private int CalcHash(T content)
+        {
+            return 0;
+        }
+    }
+}
